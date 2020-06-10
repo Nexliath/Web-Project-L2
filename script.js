474aa53b29650316ac8ce6da4d31203bb54d9e80
Nexliath <victor.garnier@efrei.net>
2020-06-10 22:32:42 +0200

    scroll to top effect

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Jeu : its your turn
 
 let motsecret;
 
@@ -18,51 +19,52 @@ mots[6] = "SHENZHEN";
 
 
 // On prend un mot au hasard en fonction de la seconde en cours
-motSecret=mots[Math.floor(Math.random() * 7)];
+motSecret = mots[Math.floor(Math.random() * 7)];
 let wordLength = motSecret.length;
 
-function changeColor(key, color){
+function changeColor(key, color) {
     key.style.backgroundColor = color;
 }
 
 
-function set(key){
+function set(key) {
 
-    if(key.style.backgroundColor == "red" || end){return;} //Avoid the color changing of the keyboard when the game is over or when the key is already colored
+    if (key.style.backgroundColor == "red" || end) {
+        return;
+    } //Avoid the color changing of the keyboard when the game is over or when the key is already colored
 
-    
+
     changeColor(key, "#26ee99");
 
     let trouve = false; //setting the key to false when the user click on it
 
-    for(let i = 0; i < wordLength; i++){
-        if(tableauMot[i].innerHTML == key.innerHTML){
+    for (let i = 0; i < wordLength; i++) {
+        if (tableauMot[i].innerHTML == key.innerHTML) {
             tableauMot[i].style.visibility = 'visible';
             trouve = true;
             letterFound++;
         }
     }
 
-    if(!trouve){
+    if (!trouve) {
         changeColor(key, "red");
         rate++;
-        document.images['pendu'].src="images/pendu_"+rate+".jpg"; // On change l'image du pendu
-        
-        if(rate == 9){
-            for(let i = 0; i < wordLength; i++){
+        document.images['pendu'].src = "images/pendu_" + rate + ".jpg"; // On change l'image du pendu
+
+        if (rate == 9) {
+            for (let i = 0; i < wordLength; i++) {
                 tableauMot[i].style.visibility = 'visible';
                 end = true;
             }
         }
-    }
-    else if(letterFound == wordLength){
+    } else if (letterFound == wordLength) {
         end = true;
     }
 
 }
 
-function display_country(country){
-    if(end == true){
+function display_country(country) {
+    if (end == true) {
         country.style.display = "block";
     }
 }
